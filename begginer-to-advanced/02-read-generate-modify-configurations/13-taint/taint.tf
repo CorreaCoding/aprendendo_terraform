217ac53f25f635e0649d430381a138df66aed63b
correacoding <amandacorreades@hotmail.com>
2025-12-15 03:14:26 -0300

    feat: understanding terraform taint or -replace

---
+++ begginer-to-advanced/02-read-generate-modify-configurations/13-taint/taint.tf
@@ -0,0 +1,22 @@
+# taint -> -replace
+# terraform apply -replace="module.ec2-instance" 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "database" {
+  ami           = data.aws_ami.myimage.id
+  instance_type = "t3.micro"
+}
+
+data "aws_ami" "myimage" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["*Linux*"]
+  }
+
+}

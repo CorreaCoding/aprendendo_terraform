ad7d7f29e1bc52e764dbae71ccfcb26a53065db4
correacoding <amandacorreades@hotmail.com>
2025-12-15 03:26:33 -0300

    feat: understanding the use of terraform plan -out file

---
+++ begginer-to-advanced/02-read-generate-modify-configurations/15-terraform-plan-file/planfile.tf
@@ -0,0 +1,18 @@
+/*---
+  This Terraform configuration creates three AWS IAM users and outputs their ARNs.
+  1. Use o comando terraform plan -out=planfile.tf para gerar um arquivo de plano.
+  2. Use o comando terraform apply planfile.tf para aplicar o plano salvo.
+---*/
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "users" {
+  name = "user-${count.index}"
+  count = 3
+}
+
+output "arn_users" {
+  value = aws_iam_user.users[*].arn
+}

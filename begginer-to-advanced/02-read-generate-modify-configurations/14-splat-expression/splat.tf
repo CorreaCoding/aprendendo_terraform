904694173e7ed1cf0e4c604f0acb522bdd88f120
correacoding <amandacorreades@hotmail.com>
2025-12-15 03:19:23 -0300

    feat: understanding splat expressions

---
+++ begginer-to-advanced/02-read-generate-modify-configurations/14-splat-expression/splat.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "users" {
+  name = "user-${count.index}"
+  count = 3
+}
+
+output "arn_users" {
+  value = aws_iam_user.users[*].arn
+}
